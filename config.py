6ac5d0dfccf14f28c846550eca937f830630a75b
jsh9965 <jsh9965@nyu.edu>
2025-11-18 12:20:01 -0500

    Made config override to avoid stale secret/uri

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
